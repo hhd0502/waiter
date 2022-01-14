8b60e90d0291bd0e15b281e16d1e47a98851aead
Duc <hoanghuuduc777@gmail.com>
2022-01-14 14:18:28 +0700

    function order service api call by retrofit

--- Lib/src/main/java/com/hutech/lib/Services/OrderService.java
+++ Lib/src/main/java/com/hutech/lib/Services/OrderService.java
@@ -1,10 +1,9 @@
 package com.hutech.lib.Services;
 
-import android.database.Observable;
-
 import com.hutech.lib.Model.OrderCreateModel;
 import com.hutech.lib.ResultModel.OrderResultModel;
 
+import io.reactivex.Observable;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
